97c48a7a43892f49279b5805ceabd653ccb21b23
Cunhao-Li <czl0111@auburn.edu>
2021-12-03 23:38:29 -0600

    Shiny: the skeleton looks fine but still has error
    Error described in comments

--- LRGroup1/app.R
+++ LRGroup1/app.R
@@ -1,33 +1,16 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
 devtools::install_github("AU-R-Programming/FinalProject")
-library(FinalProject)
-
-
+library(lmG1)
 library(ggplot2)
 
-
-
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
     titlePanel("Simple Linear Regression Tool"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-
+            # Ask the user for inputs X, y, and a
+            # Future updates: matrix indexes, maybe a more clever input field?
             textInput("X", "X", value = "1, 2, 3, 4, 5"),
             textInput("y", "y", value = "14, 24, 32, 55, 61"),
             textInput("a", "a", value = "0.05")
@@ -49,7 +32,9 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+
+
+
 server <- function(input, output) {
 
     output$beta <- (lmG1(y = input$y, X = input$X, a = input$a))$beta
@@ -58,26 +43,35 @@ server <- function(input, output) {
     output$Fstat <- (lmG1(y = input$y, X = input$X, a = input$a))$f_stat
     output$Pf <- (lmG1(y = input$y, X = input$X, a = input$a))$pf_value
 
-    output$RvsF <- renderPlot({
-        # generate bins based on input$bins from ui.R
+    output$RvsF <- renderPlot({ # Residuals vs Fitted plot
 
-        lmG1(X = input$X, y = input$y)
-    })
-
-    output$RQQ <- renderPlot({
-
+        ggplot(mapping = aes(x = y_hat, y = residual)) +
+            labs(title = 'Residuals vs Fitted', x = 'Fitted values', y = 'Residuals') +
+            geom_point() + geom_smooth()
 
     })
 
-    output$HistR <- renderPlot({
+    output$RQQ <- renderPlot({ # Residual QQ-plot
 
-
+        ggplot(mapping = aes(sample = residual)) +
+            labs(title = 'Residual QQ-plot', x = 'Theoretical Quantiles', y = 'Sample Quantiles') +
+            stat_qq()
     })
 
+    output$HistR <- renderPlot({ # Histogram of Residuals
 
-
+        ggplot(mapping = aes(x = residual)) +
+            geom_histogram(fill = 'steelblue', color = 'black') +
+            labs(title = 'Histogram of Residuals', x = 'Residuals', y = 'Frequency')
+    })
 
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+
+# Error with this version: Can't access reactive value 'y' outside of reactive consumer
+# The Shiny App closes by itself after running
+# This should have something to do with reactive variables (y, as pointed out by the program; and possibly X and a as well)
+# Can't work out a solution for this problem
